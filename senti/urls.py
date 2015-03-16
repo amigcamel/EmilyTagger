3612a99cab3f2e578cbd5adb57784f233b9916a6
Tsun-Jui Liu <Aji@MBP.local>
2015-03-16 23:17:09 +0800

    fix some issues

--- senti/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from . import views
-
-
-urlpatterns = patterns('',
-    url('^signin/$', views.signin, name='signin'),
-    url('^signout/$', views.signout, name='signout'),
-)
