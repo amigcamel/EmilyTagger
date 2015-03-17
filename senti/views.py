948dfd77c35917015af41ee54b17b90a39ed048d
Tsun-Jui Liu <Aji@MBP.local>
2015-03-17 13:02:12 +0800

    move cat-box to front-end

--- senti/views.py
+++ senti/views.py
@@ -8,39 +8,38 @@ from .mongo import *
 from .ref import senti_ref
 from urllib import unquote
 import re
+from misc.templatetags.senti_tag import txt_with_color
+import json
 
 
 def main(request, current_page=None):
     return render_to_response('senti_main.html', {'senti_ref': senti_ref}, context_instance=RequestContext(request))
-    if current_page is None:
-        current_page = request.COOKIES.get('last_open_page', 1)
 
-    if int(current_page) < 1:
-        return HttpResponseRedirect(reverse('index'))
-    elif int(current_page) >= 1 and int(current_page) <= 499:
-        db_name = 'senti_news'
-    elif int(current_page) > 499 and int(current_page) <= 899:
-        db_name = 'senti_ptt'
-    elif int(current_page) > 899:
-        return HttpResponseRedirect(reverse('index'))
+    # if current_page is None:
+    #     current_page = request.COOKIES.get('last_open_page', 1)
 
-    cat, url = find_next_id(int(current_page), db_name)
-    dic = dict()
-    dic['current_page'] = int(current_page) + 1
+    # if int(current_page) < 1:
+    #     return HttpResponseRedirect(reverse('index'))
+    # elif int(current_page) >= 1 and int(current_page) <= 499:
+    #     db_name = 'senti_news'
+    # elif int(current_page) > 499 and int(current_page) <= 899:
+    #     db_name = 'senti_ptt'
+    # elif int(current_page) > 899:
+    #     return HttpResponseRedirect(reverse('index'))
 
-    doc = c[db_name][cat].find({'_id': url}, {'content': 1}).next()
-    dic['text'] = doc['content']
-    text_id = dic['text_id'] = doc['_id']
-    dic['senti_ref'] = senti_ref
-    dic['pairs'] = read_pairs(text_id, request.user.username)
-    dic['pairs_by_cat'] = read_by_cat(text_id, request.user.username)
-    res = render_to_response('senti_main.html', dic, context_instance=RequestContext(request))
-    # res.set_cookie('last_open_page', current_page)
-    return res
+    # cat, url = find_next_id(int(current_page), db_name)
+    # dic = dict()
+    # dic['current_page'] = int(current_page) + 1
 
-
-from misc.templatetags.senti_tag import txt_with_color
-import json
+    # doc = c[db_name][cat].find({'_id': url}, {'content': 1}).next()
+    # dic['text'] = doc['content']
+    # text_id = dic['text_id'] = doc['_id']
+    # dic['senti_ref'] = senti_ref
+    # dic['pairs'] = read_pairs(text_id, request.user.username)
+    # dic['pairs_by_cat'] = read_by_cat(text_id, request.user.username)
+    # res = render_to_response('senti_main.html', dic, context_instance=RequestContext(request))
+    # # res.set_cookie('last_open_page', current_page)
+    # return res
 
 
 def get_cand_text(request, page_num):
