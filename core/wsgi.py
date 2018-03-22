3b822816b11e151317d75005125ab12180a6ef23
amigcamel <amigcamel@gmail.com>
2018-03-22 13:50:39 +0800

    fix typo

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Emily.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
